ca8224f0064619390b5866c9be09c861ce2a518a
fujikyo-1214 <hatumike@icloud.com>
2021-01-20 19:25:33 +0900

    issue6 訂正版

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -20,10 +20,8 @@ class CalendarsController < ApplicationController
 
   def get_week
     wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
-    @today_wday = Date.today.wday
-
+    
     @todays_date = Date.today
-
 
     @week_days = []
 
@@ -35,12 +33,12 @@ class CalendarsController < ApplicationController
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
 
-      wday_num = @today_wday
+      wday_num = (@todays_date).wday
       if (wday_num >= 7)
         wday_num = wday_num -7
       end
 
-      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans, wdays: @today_wday}
+      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans, wdays: wdays[(@todays_date + x).wday]}
       @week_days.push(days)
     end
   end
